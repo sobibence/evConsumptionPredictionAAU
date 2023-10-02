3b1f9c42b5d4bf2ce3c1cf00ea1fbdb68d163e35
Simeon Kolev <simeonkolev99@gmail.com>
2023-10-03 01:47:01 +0200

    Initial project structure setup.
    - Created layers and test project
    - Added gitignore

---
+++ EVCP.Api.E2ETests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace EVCP.Api.E2ETests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
