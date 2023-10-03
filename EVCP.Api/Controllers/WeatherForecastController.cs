aaaff49121c714866a54101cdd0e43a2d9642dbe
Simeon Kolev <simeonkolev99@gmail.com>
2023-10-03 11:32:37 +0200

    Setup logging

--- EVCP.Api/Controllers/WeatherForecastController.cs
+++ EVCP.Api/Controllers/WeatherForecastController.cs
@@ -11,11 +11,8 @@ namespace EVCP.Api.Controllers
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController()
         {
-            _logger = logger;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
