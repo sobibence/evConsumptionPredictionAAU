851a16ef7d4afedf087a1c19430211c2b503e826
Simeon Kolev <simeonkolev99@gmail.com>
2023-10-03 11:32:49 +0200

    Example E2E test

--- EVCP.Api.E2ETests/UnitTest1.cs
+++ EVCP.Api.E2ETests/UnitTest1.cs
@@ -1,16 +1,24 @@
+using System.Net;
+
 namespace EVCP.Api.E2ETests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public async Task GetWeatherForecast()
         {
-            Assert.Pass();
+            // Arrange
+            var client = new HttpClient()
+            {
+                BaseAddress = new Uri("https://localhost:7251/")
+            };
+
+            // Act
+            var response = await client.GetAsync("/WeatherForecast");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
         }
     }
 }
