e98fffdb56638fd9b926537c4d3b092659534dc9
Simeon Kolev <simeonkolev99@gmail.com>
2023-10-19 23:39:13 +0200

    Use OpenWeather service in WeatherController.
    Also updated WeatherDataGenerator to accommodate new WeatherData model.

--- EVCP.Domain/Services/WeatherDataGenerator.cs
+++ EVCP.Domain/Services/WeatherDataGenerator.cs
@@ -2,85 +2,24 @@
 
 namespace EVCP.Domain.Services
 {
-    public class WeatherDataGenerator
+    public static class WeatherDataGenerator
     {
-        private readonly Random random;
-        private HourlyWeatherData previousHourData = new(0, false, false, 0, 0, 0, 0, 0);
+        private static readonly Random random = new();
 
-        public WeatherDataGenerator()
+        public static WeatherData GenerateWeatherData(DateTime date)
         {
-            random = new Random();
-        }
-
-        public WeatherData GenerateWeatherData(DateTime date)
-        {
-            var hourlyData = new List<HourlyWeatherData>();
-            for (int i = 0; i < 24; i++)
-            {
-                var hourlyDatum = GenerateHourlyWeatherData(i);
-                hourlyData.Add(hourlyDatum);
-                previousHourData = hourlyDatum;
-            }
-
-            return new WeatherData(date, hourlyData);
-        }
-
-        private HourlyWeatherData GenerateHourlyWeatherData(int hour)
-        {
-            return new HourlyWeatherData
+            return new WeatherData
             (
-                hour: hour,
-                isRaining: GenerateRandomWithPersistence(previousHourData.IsRaining, 0.3),
-                isSnowing: GenerateRandomWithPersistence(previousHourData.IsSnowing, 0.1),
-                windSpeed: GenerateRandomWithPersistence(previousHourData.WindSpeed, 10, 0, 30),
-                windDirection: GenerateRandomWithPersistence(previousHourData.WindDirection, 30, 0, 360),
-                humidity: GenerateRandomWithPersistence(previousHourData.Humidity, 5, 20, 80),
-                temperature: GenerateRandomWithPersistence(previousHourData.Temperature, 3, -10, 40),
-                precipitation: GenerateRandomWithPersistence(previousHourData.Precipitation, 0.2, 0, 1.0)
+                at: date,
+                isRaining: random.NextDouble() < 0.3, // 30% chance of rain
+                isSnowing: random.NextDouble() < 0.1, // 10% chance of snow
+                windSpeed: random.Next(0, 30), // Wind speed in kph (0 to 50)
+                windDirection: random.Next(0, 360), // Wind direction in degrees (0 to 359)
+                fog: random.Next(0, 10), // Fog percentage (0% to 20%)
+                humidity: random.Next(20, 80), // Humidity percentage (20% to 80%)
+                temperature: random.Next(-10, 40), // Temperature in Celsius (-10°C to 40°C)
+                precipitation: random.Next(0, 10) / 10.0 // Precipitation in mm (0 to 1.0 mm)
             );
-        }
-
-        private bool GenerateRandomWithPersistence(bool previousValue, double probability)
-        {
-            if (random.NextDouble() < probability)
-            {
-                return previousValue;
-            }
-            return random.NextDouble() < probability;
-        }
-
-        private int GenerateRandomWithPersistence(int previousValue, int maxChange, int minValue, int maxValue)
-        {
-            int change = random.Next(-maxChange, maxChange + 1);
-            int newValue = previousValue + change;
-
-            if (newValue < minValue)
-            {
-                newValue = minValue;
-            }
-            else if (newValue > maxValue)
-            {
-                newValue = maxValue;
-            }
-
-            return newValue;
-        }
-
-        private double GenerateRandomWithPersistence(double previousValue, double maxChange, double minValue, double maxValue)
-        {
-            double change = (random.NextDouble() * 2 - 1) * maxChange;
-            double newValue = previousValue + change;
-
-            if (newValue < minValue)
-            {
-                newValue = minValue;
-            }
-            else if (newValue > maxValue)
-            {
-                newValue = maxValue;
-            }
-
-            return newValue;
         }
     }
 }
