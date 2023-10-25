b25a8555de75ea75290bcb596a4da5f72b1bbf26
Simeon Kolev <43152543+jokerofun@users.noreply.github.com>
2023-10-25 23:31:01 +0200

    Weather service (#3)
    * WIP: Weather from OpenWeather and Weather generator service.
    
    * Removed WeatherForecast example classes.
    
    * Updated WeatherData model.
    
    * OpenWeather service.
    
    * Use OpenWeather service in WeatherController.
    
    Also updated WeatherDataGenerator to accommodate new WeatherData model.

---
+++ OpenWeather/OpenWeatherService.cs
@@ -0,0 +1,66 @@
+﻿using EVCP.Domain.Models;
+using EVCP.OpenWeather;
+using Newtonsoft.Json;
+
+namespace OpenWeather
+{
+    public class OpenWeatherService
+    {
+        private readonly string apiKey = "";
+
+        public OpenWeatherService()
+        {
+        }
+
+        public async Task<WeatherData> GetWeatherAsync(double lat, double lon)
+        {
+            string url = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apiKey}&units=metric";
+
+            using var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            var content = response.Content.ReadAsStringAsync().Result;
+
+            var openWeatherResponse = JsonConvert.DeserializeObject<OpenWeatherResponse>(content);
+
+            return Map(openWeatherResponse);
+        }
+
+        private static WeatherData Map(OpenWeatherResponse openWeatherResponse)
+        {
+            return new WeatherData
+                (
+                    at: DateTimeOffset.FromUnixTimeSeconds(openWeatherResponse.Dt).LocalDateTime,
+                    isRaining: openWeatherResponse.Weather.Exists(w => w.Main == "Rain"),
+                    isSnowing: openWeatherResponse.Weather.Exists(w => w.Main == "Snow"),
+                    windSpeed: (int)openWeatherResponse.Wind.Speed,
+                    windDirection: openWeatherResponse.Wind.Deg,
+                    fog: CalculateFog(openWeatherResponse.Visibility, openWeatherResponse.Weather.Select(w => w.Id).ToArray()),
+                    humidity: openWeatherResponse.Main.Humidity,
+                    temperature: (int)openWeatherResponse.Main.Temp,
+                    precipitation: openWeatherResponse.Rain?.ThreeHours ?? 0
+                );
+        }
+
+        private static int CalculateFog(int visibility, int[] weatherCodes)
+        {
+            var random = new Random();
+
+            if (visibility < 1000) // Visibility in meters
+            {
+                return random.Next(50, 100);
+            }
+
+            if (weatherCodes.Contains(701) || weatherCodes.Contains(741)) // Mist or fog
+            {
+                return random.Next(20, 100);
+            }
+
+            if (weatherCodes.Contains(721)) // Haze
+            {
+                return random.Next(10, 30);
+            }
+
+            return 0;
+        }
+    }
+}
