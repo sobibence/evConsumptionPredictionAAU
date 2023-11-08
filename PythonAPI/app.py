41d85a701668db233908082eb0138a334783f593
Malthe122 <malthe122@gmail.com>
2023-11-08 21:00:13 +0100

    fixed requirements, fixed code, added postman collection for testing. Endpoint now works

--- PythonAPI/app.py
+++ PythonAPI/app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import sys
 
 app = Flask(__name__)
 
-@app.route('/consumption/<meters>', methods=['GET'])
+@app.route('/consumption/', methods=['GET'])
 
 def consumption():
-    return "{meters}"
+    # This should be of type modelInput, but there is no typing of variables in python
+    print(request.get_json())
+    input = request.get_json()
+    return str(input['meters'])
 
 if __name__ == '__main__':
     app.run(debug=True)
