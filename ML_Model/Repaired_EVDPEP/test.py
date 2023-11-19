9be433253f307b1fa08ce60ded1dcad00bb72eed
sobibence <sobibence@gmail.com>
2023-11-19 17:04:16 +0000

    got final model

--- ML_Model/Repaired_EVDPEP/test.py
+++ ML_Model/Repaired_EVDPEP/test.py
@@ -8,7 +8,7 @@ preloaded_model = None
 def load_model():
     print("Startup")
     global preloaded_model 
-    preloaded_model = tf.keras.models.load_model('output/v3_dnn_mse_al_speed_limit_1_adam_epoch=40/v3_last-only_epoch=40.h5', custom_objects={'gaussian_nll': losses.gaussian_nll})
+    preloaded_model = tf.keras.models.load_model('/home/sobibence/project/evConsumptionPredictionAAU/ML_Model/Repaired_EVDPEP/output/v3_lstm_mse_al_speed_limit_1_adam_epoch=40/v3_last-only_epoch=40.h5', custom_objects={'gaussian_nll': losses.gaussian_nll})
     input_layer = preloaded_model.layers[0]  # Assuming the input layer is the first layer
     input_shape = input_layer.input_shape
     # input_data_type = input_layer.input_dtype
@@ -20,6 +20,8 @@ def load_model():
 print("test")
 
 load_model()
-predictions = preloaded_model.predict(np.random.rand(1,550,30))
-print("2")
+shape = (1,550,30)
+values = np.full(shape, 0)
+print(values)
+predictions = preloaded_model.predict(values)
 print(predictions)
