6574859c72fbf4815bfd05bb4aa40b26eca48fa7
sobibence <sobibence@gmail.com>
2023-11-23 02:45:02 +0000

    finally connected to db

---
+++ EVCP.RouteFinding/DataBaseConnector.cs
@@ -0,0 +1,30 @@
+﻿using EVCP.DataAccess;
+using EVCP.Domain.Models;
+using EVCP.Domain.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EVCP.RouteFinding;
+
+public class DataBaseConnector
+{
+
+    private static readonly object Instancelock = new object();
+    private static DataBaseConnector instance = new DataBaseConnector();
+    public static DataBaseConnector Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new DataBaseConnector();
+            }
+            return instance;
+        }
+    }
+
+    private DataBaseConnector()
+    {
+        
+    }
+}
---
+++ EVCP.RouteFinding/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EVCP.DataAccess;
+using EVCP.Domain.Models;
+using EVCP.Domain.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+        
+        builder.Services.AddSingleton<DapperContext>();
+        builder.Logging.AddConsole();
+        builder.Services.AddTransient<IEdgeRepository, EdgeRepository>();
+        builder.Services.AddTransient<IFEstConsumptionRepository, FEstConsumptionRepository>();
+        builder.Services.AddTransient<IFRecordedTravelRepository, FRecordedTravelRepository>();
+        builder.Services.AddTransient<INodeRepository, NodeRepository>();
+        builder.Services.AddTransient<IProducerRepository, ProducerRepository>();
+        builder.Services.AddTransient<IVehicleModelRepository, VehicleModelRepository>();
+        builder.Services.AddTransient<IVehicleTripStatusRepository, VehicleTripStatusRepository>();
+        builder.Services.AddTransient<IWeatherRepository, WeatherRepository>();
+        
+    
+        IHost host = builder.Build();
+        
+        
+        Node node = new Node();
+        List<Node> listOfNodes = new() { node };
+        host.Services.GetService<INodeRepository>().Create(listOfNodes);
+        
+        var asd = await host.Services.GetService<INodeRepository>().GetByIdAsync(0);
+        
+        
+        
+        
+        
+        
+        
+        host.Run();
+        
+    }
+
+
+}
