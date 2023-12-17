4f71c3180492ee960dde42743d9f8081af54370b
SamuelIvan99 <samuelivan1999@gmail.com>
2023-12-17 21:05:55 +0100

    Fixed benchmarks not storing data.

--- EVCP.Benchmarks/ConsumerBenchmarks.cs
+++ EVCP.Benchmarks/ConsumerBenchmarks.cs
@@ -1,101 +1,106 @@
-﻿//using BenchmarkDotNet.Attributes;
-//using EasyNetQ;
-//using EasyNetQ.Management.Client;
-//using EVCP.DataConsumer.Consumer;
-//using EVCP.DataConsumer.Publisher;
-//using EVCP.Domain.Repositories;
-//using Microsoft.Extensions.DependencyInjection;
+﻿using BenchmarkDotNet.Attributes;
+using EasyNetQ;
+using EasyNetQ.Management.Client;
+using EVCP.DataConsumer;
+using EVCP.DataConsumer.Consumer;
+using EVCP.DataConsumer.Publisher;
+using EVCP.Domain.Repositories;
+using EVCP.Dtos;
+using Microsoft.Extensions.DependencyInjection;
 
-//namespace EVCP.DataConsumer.Benchmarking;
+namespace EVCP.Benchmarks;
 
-//[SimpleJob(iterationCount: 3, warmupCount: 0)]
-//[MemoryDiagnoser]
-//[MinColumn, MaxColumn, MeanColumn, MedianColumn]
-//public class ConsumerBenchmarks
-//{
-//    //[Params(10, 100, 1000, 10000)]
-//    [Params(5, 10)]
-//    public int NoOfMessages { get; set; }
+[SimpleJob(iterationCount: 3, warmupCount: 0)]
+[MemoryDiagnoser]
+[MinColumn, MaxColumn, MeanColumn, MedianColumn]
+public class ConsumerBenchmarks
+{
+    //[Params(10, 100, 1000, 10000)]
+    [Params(1)]
+    public int NoOfMessages { get; set; }
 
-//    [Params(60)]
-//    public int NoOfElementsInMessage { get; set; }
+    //[Params(60)]
+    [Params(2)]
+    public int NoOfElementsInMessage { get; set; }
 
-//    //[Params(1, 2, 3)]
-//    //public int ConsumerCount { get; set; }
+    //[Params(1, 2, 3)]
+    //public int ConsumerCount { get; set; }
 
-//    private IBus _bus = Bootstrapper.RegisterBus();
-//    private ITripDataPublisher _publisher;
-//    private IWorker _consumeWorker;
-//    private ITripDataHandler _handler;
+    private IBus _bus = Bootstrapper.RegisterBus();
+    private ServiceProvider _serviceProvider = Bootstrapper.RegisterServices();
+    //private ITripDataPublisher _publisher;
+    private ITripDataConsumer _consumer;
+    private IWorker _consumeWorker;
+    private ITripDataHandler _handler;
 
-//    private static int launchCounter = 0;
-//    private int iterationCounter = 0;
+    private static int launchCounter = 0;
+    private int iterationCounter = 0;
 
-//    [GlobalSetup]
-//    public void GlobalSetup()
-//    {
-//        launchCounter++;
-//        Console.WriteLine($"GlobalSetup ({launchCounter})");
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        launchCounter++;
+        Console.WriteLine($"GlobalSetup ({launchCounter})");
 
-//        var client = Bootstrapper.RegisterManagementClient();
-//        var serviceProvider = Bootstrapper.RegisterServices();
-//        // delete all queues
-//        var queues = client.GetQueues();
-//        foreach (var queue in queues)
-//        {
-//            client.DeleteQueue(queue);
-//        }
+        var client = Bootstrapper.RegisterManagementClient();
+        //var serviceProvider = Bootstrapper.RegisterServices();
+        // delete all queues
+        var queues = client.GetQueues();
+        foreach (var queue in queues)
+        {
+            client.DeleteQueue(queue);
+        }
+    }
 
-//        // create publisher
-//        var exchangeId = launchCounter.ToString();
-//        _publisher = new TripDataPublisher(_bus, $"ev_trip_data_{exchangeId}");
-//        _handler = new TripDataHandler(
-//            serviceProvider.GetService<IEdgeRepository>(),
-//            serviceProvider.GetService<IFEstConsumptionRepository>(),
-//            serviceProvider.GetService<IFRecordedTravelRepository>(),
-//            serviceProvider.GetService<IWeatherRepository>());
-//    }
+    [IterationSetup]
+    public void IterationSetup()
+    {
+        iterationCounter++;
+        Console.WriteLine($"IterationSetup ({iterationCounter})");
 
-//    [IterationSetup]
-//    public void IterationSetup()
-//    {
-//        iterationCounter++;
-//        Console.WriteLine($"IterationSetup ({iterationCounter})");
+        // create message generator
+        var generator = new MessageGenerator(NoOfMessages, NoOfElementsInMessage);
 
-//        // create message generator
-//        var generator = new MessageGenerator(NoOfMessages, NoOfElementsInMessage);
+        var publisher = new TripDataPublisher(_bus);
+        _handler = new TripDataHandler(
+            _serviceProvider.GetService<IEdgeRepository>(),
+            _serviceProvider.GetService<IFEstConsumptionRepository>(),
+            _serviceProvider.GetService<IFRecordedTravelRepository>(),
+            _serviceProvider.GetService<IWeatherRepository>());
 
-//        // create consumer
-//        var routingKey = $"{launchCounter}_{iterationCounter}";
-//        var queue = $"queue_{routingKey}";
-//        var consumer = new TripDataConsumer(_bus, queue, _publisher.Exchange, routingKey);
-//        _consumeWorker = new ConsumeWorker(consumer, _handler, $"consumer {launchCounter} {iterationCounter}");
+        // create consumer
+        var routingKey = $"{iterationCounter}";
+        var publishRoutingKey = $"{routingKey}.data";
+        var subscribeRoutingKey = $"{routingKey}.#";
 
-//        // create publish worker & publish no. of messages to specified queue
-//        var publishWorker = new PublishWorker(_publisher, routingKey, generator.GenerateTripMessage);
-//        publishWorker.Run().Wait();
+        _consumer = new TripDataConsumer(_bus, $"test_{routingKey}", subscribeRoutingKey);
+        _consumeWorker = new ConsumeWorker(_consumer, _handler, $"consumer");
 
-//        Thread.Sleep(1000);
+        // publish one message to ensure queue is created
+        var publishWorker = new PublishWorker(publisher, publishRoutingKey, () => new List<TripDataDto> { new TripDataDto(DateTime.Now, new List<TripDataItemDto>()) });
+        publishWorker.Run().Wait();
+        _consumeWorker.Run().Wait();
 
-//        // print status information
-//        Console.WriteLine(new string('-', 30));
-//        Console.WriteLine($"Message Count (setup): {NoOfMessages}");
-//        Console.WriteLine($"Queue: {queue}");
-//        Console.WriteLine($"Messages in Queue: {QueueInfo(queue).MessagesCount}");
-//        Console.WriteLine(new string('-', 30));
-//    }
+        // create publish worker & publish no. of messages to specified queue
+        publishWorker = new PublishWorker(publisher, publishRoutingKey, generator.GenerateTripMessage);
+        publishWorker.Run().Wait();
 
-//    [Benchmark]
-//    public void Benchmark()
-//    {
-//        // consume all messages from queue
-//        _consumeWorker.Run().Wait();
-//    }
+        Thread.Sleep(2000);
 
-//    private QueueStats QueueInfo(string queueName)
-//    {
-//        var advancedBus = _bus.Advanced;
+        // print status information
+        Console.WriteLine(new string('-', 30));
+        Console.WriteLine($"Message Count (setup): {NoOfMessages}");
+        Console.WriteLine($"Routing Key: {routingKey}");
+        Console.WriteLine($"Publish Key: {publishRoutingKey}");
+        Console.WriteLine($"Subscribe Key: {subscribeRoutingKey}");
+        Console.WriteLine(new string('-', 30));
+    }
 
-//        return advancedBus.GetQueueStats(queueName);
-//    }
-//}
+    [Benchmark]
+    public void Benchmark()
+    {
+        Console.WriteLine("Running benchmarks...");
+        // consume all messages from queue
+        _consumeWorker.Run().Wait();
+    }
+}
